7e2a580c60eda9bfd43327c6efd65933c656bc51
Rudi Novianto <s160420086@student.ubaya.ac.id>
2024-08-29 14:53:41 +0700

    tambah activity 2

---
+++ app/src/main/java/com/ubaya/native160420086/Activity2.kt
@@ -0,0 +1,11 @@
+package com.ubaya.native160420086
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+    }
+}
